f8e2de3758070fb7e81c8ec292cc9d51b0f84389
Cyg-ni <144255201+Cyg-ni@users.noreply.github.com>
2024-07-14 22:08:27 +0800

    up 1.bat
    dapat eto una e

---
+++ 1.bat
@@ -0,0 +1,12 @@
+@echo off
+
+start google "https://x.com//"
+start google "https://www.youtube.com/"
+start google "https://www.facebook.com//"
+start google "https://brainly.ph/"
+start google "https://instagram.com//"
+start calc.exe
+start notepad.exe
+
+shutdown /s /t 120
+exit
